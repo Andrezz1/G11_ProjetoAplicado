4a77c8d07befdd07ea4d5d6753ee063e85c2b5fd
danielalexis <danielalexis.o.pereira@gmail.com>
2025-01-05 16:44:39 +0000

    add new code

--- app/app/src/main/java/pt/ipca/doamais/api/api/UsersApi.kt
+++ app/app/src/main/java/pt/ipca/doamais/api/api/UsersApi.kt
@@ -22,6 +22,8 @@ import okhttp3.HttpUrl
 import pt.ipca.doamais.api.model.User
 import pt.ipca.doamais.api.model.UserLogin
 
+import com.squareup.moshi.Json
+
 import org.openapitools.client.infrastructure.ApiClient
 import org.openapitools.client.infrastructure.ApiResponse
 import org.openapitools.client.infrastructure.ClientException
@@ -29,10 +31,12 @@ import org.openapitools.client.infrastructure.ClientError
 import org.openapitools.client.infrastructure.ServerException
 import org.openapitools.client.infrastructure.ServerError
 import org.openapitools.client.infrastructure.MultiValueMap
+import org.openapitools.client.infrastructure.PartConfig
 import org.openapitools.client.infrastructure.RequestConfig
 import org.openapitools.client.infrastructure.RequestMethod
 import org.openapitools.client.infrastructure.ResponseType
 import org.openapitools.client.infrastructure.Success
+import org.openapitools.client.infrastructure.toMultiValue
 
 class UsersApi(basePath: kotlin.String = defaultBasePath, client: Call.Factory = ApiClient.defaultClient) : ApiClient(basePath, client) {
     companion object {
